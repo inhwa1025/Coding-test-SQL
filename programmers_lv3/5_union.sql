9f2a0f6161cc564e878b6f7a9981cb6d44f975cc
inhwa1025 <inhwa9901@gmail.com>
2022-05-19 14:51:47 +0900

    5. union

---
+++ programmers_lv3/5_union.sql
@@ -0,0 +1,47 @@
+SELECT 0 hour,sum(if(hour(datetime) = 0,1,0)) count from animal_outs
+union
+SELECT 1 hour,sum(if(hour(datetime) = 1,1,0)) count from animal_outs
+union
+SELECT 2 hour,sum(if(hour(datetime) = 2,1,0)) count from animal_outs
+union
+SELECT 3 hour,sum(if(hour(datetime) = 3,1,0)) count from animal_outs
+union
+SELECT 4 hour,sum(if(hour(datetime) = 4,1,0)) count from animal_outs
+union
+SELECT 5 hour,sum(if(hour(datetime) = 5,1,0)) count from animal_outs
+union
+SELECT 6 hour,sum(if(hour(datetime) = 6,1,0)) count from animal_outs
+union
+SELECT 7 hour,sum(if(hour(datetime) = 7,1,0)) count from animal_outs
+union
+SELECT 8 hour,sum(if(hour(datetime) = 8,1,0)) count from animal_outs
+union
+SELECT 9 hour,sum(if(hour(datetime) = 9,1,0)) count from animal_outs
+union
+SELECT 10 hour,sum(if(hour(datetime) = 10,1,0)) count from animal_outs
+union
+SELECT 11 hour,sum(if(hour(datetime) = 11,1,0)) count from animal_outs
+union
+SELECT 12 hour,sum(if(hour(datetime) = 12,1,0)) count from animal_outs
+union
+SELECT 13 hour,sum(if(hour(datetime) = 13,1,0)) count from animal_outs
+union
+SELECT 14 hour,sum(if(hour(datetime) = 14,1,0)) count from animal_outs
+union
+SELECT 15 hour,sum(if(hour(datetime) = 15,1,0)) count from animal_outs
+union
+SELECT 16 hour,sum(if(hour(datetime) = 16,1,0)) count from animal_outs
+union
+SELECT 17 hour,sum(if(hour(datetime) = 17,1,0)) count from animal_outs
+union
+SELECT 18 hour,sum(if(hour(datetime) = 18,1,0)) count from animal_outs
+union
+SELECT 19 hour,sum(if(hour(datetime) = 19,1,0)) count from animal_outs
+union
+SELECT 20 hour,sum(if(hour(datetime) = 20,1,0)) count from animal_outs
+union
+SELECT 21 hour,sum(if(hour(datetime) = 21,1,0)) count from animal_outs
+union
+SELECT 22 hour,sum(if(hour(datetime) = 22,1,0)) count from animal_outs
+union
+SELECT 23 hour,sum(if(hour(datetime) = 23,1,0)) count from animal_outs
